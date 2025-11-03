e82bff012c0bf59fec36ddd4458031ffc837592c
architeketh <architek.eth@gmail.com>
2025-11-02 22:54:28 -0600

    Increase max width of main container in App component

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   }, [items])
 
   return (
-    <div className="p-4 max-w-4xl mx-auto">
+    <div className="p-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">Ecommerce Inventory System</h1>
       <Dashboard items={items} />
       <InventoryForm items={items} setItems={setItems} />
